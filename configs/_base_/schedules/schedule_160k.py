4e58894462984f70f2289dde303188d94bba3dbc
要谦年人 <420480755@qq.com>
2023-05-05 22:11:58 +0800

    Set save_best and change checkpoint_interval to 160000.

--- configs/_base_/schedules/schedule_160k.py
+++ configs/_base_/schedules/schedule_160k.py
@@ -5,5 +5,5 @@ optimizer_config = dict()
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
 # runtime settings
 runner = dict(type='IterBasedRunner', max_iters=160000)
-checkpoint_config = dict(by_epoch=False, interval=16000)
-evaluation = dict(interval=4000, metric='mIoU')
+checkpoint_config = dict(by_epoch=False, interval=160000)
+evaluation = dict(interval=4000, metric='mIoU',save_best='mIoU')
