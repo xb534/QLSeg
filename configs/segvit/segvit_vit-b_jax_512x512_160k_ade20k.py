49d8118a7aaac7fd7adc2d1825116d5960531cca
xiebin <420480755@qq.com>
2023-05-11 16:33:14 +0800

    Add new hyperparameter model.decode_head.num_ps_layers=6,

--- configs/segvit/segvit_vit-b_jax_512x512_160k_ade20k.py
+++ configs/segvit/segvit_vit-b_jax_512x512_160k_ade20k.py
@@ -15,6 +15,7 @@ model = dict(
     decode_head=dict(
         use_stages=len(out_indices),
         num_atm_layers=num_atm_layers,
+        num_ps_layers=6,
         compress_c=10,
         loss_decode=dict(
             type='ATMLoss', num_classes=150, dec_layers=num_atm_layers, loss_weight=1.0),
