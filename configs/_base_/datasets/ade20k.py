9a921c8c9f1d4a3b525a0bd5b5c018bbfa4322d4
xiebin <420480755@qq.com>
2023-05-19 20:15:36 +0800

    1.Fix bug.Can not eval on scale of [0.5,0.75].

--- configs/_base_/datasets/ade20k.py
+++ configs/_base_/datasets/ade20k.py
@@ -24,7 +24,7 @@ test_pipeline = [
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', keep_ratio=True, min_size=512),
             dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='ImageToTensor', keys=['img']),
